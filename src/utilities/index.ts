6aef98ff25169927e61d10f08e150b80b9b5b36a
Enjoywater <heungsoo1@gmail.com>
2023-02-23 22:24:23 +0900

    [REMOVE] 미사용 유틸함수 제거

--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -21,25 +21,3 @@ export const checkPassword = (password: string) => {
   const passwordCheck = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,30}/;
   return passwordCheck.test(password);
 };
-
-export const setSession = (key: string, value: any, expirationInMin: number) => {
-  const expirationDate = new Date(new Date().getTime() + 60000 * expirationInMin);
-  const newValue = {
-    value,
-    expirationDate: expirationDate.toISOString(),
-  };
-  window.sessionStorage.setItem(key, JSON.stringify(newValue));
-};
-
-export const getSession = (key: string) => {
-  const stringValue = window.sessionStorage.getItem(key);
-  if (stringValue !== null) {
-    const value = JSON.parse(stringValue);
-    const expirationDate = new Date(value.expirationDate);
-    if (expirationDate > new Date()) {
-      return value.value;
-    }
-    window.sessionStorage.removeItem(key);
-  }
-  return null;
-};
