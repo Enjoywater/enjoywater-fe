48674525867a1f158d92ab8f781947e1bfab8d98
Enjoywater <heungsoo1@gmail.com>
2023-02-25 00:06:51 +0900

    [FIX] 유틸함수 내부 줄바꿈 수정

--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -14,11 +14,13 @@ export const parseQueryString = (search: string): Record<string, string> =>
 
 export const checkUserId = (id: string) => {
   const idCheck = /^[A-Za-z0-9]{5,30}$/;
+
   return idCheck.test(id);
 };
 
 export const checkPassword = (password: string) => {
   const passwordCheck = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,30}$/;
+
   return passwordCheck.test(password);
 };
 
