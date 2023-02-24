ca88b087424f0b549db430c463ee668fa6697839
Enjoywater <heungsoo1@gmail.com>
2023-02-25 00:05:38 +0900

    [FIX] 상품상세 이미지 next image로 수정

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import { dehydrate, QueryClient } from 'react-query';
 
+import Image from 'next/image';
+
 import {
   fetchProductDetail,
   useGetProductDetailQuery,
@@ -19,7 +21,9 @@ const ProductDetailPage: NextPage<ProductDetailProps> = ({ id }: ProductDetailPr
   const { thumbnail, name, price } = product;
   return (
     <>
-      <Thumbnail src={thumbnail || '/defaultThumbnail.jpg'} />
+      <Thumbnail>
+        <Image alt='product' layout='fill' src={thumbnail || '/defaultThumbnail.jpg'} />
+      </Thumbnail>
       <ProductInfoWrapper>
         <Name>{name}</Name>
         <Price>
@@ -46,7 +50,8 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 
 export default ProductDetailPage;
 
-const Thumbnail = styled.img`
+const Thumbnail = styled.div`
+  position: relative;
   width: 100%;
   height: 420px;
 `;
